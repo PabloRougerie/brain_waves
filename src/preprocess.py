16538b4cb50cd30e52c0987d8aadccebd82fa7bf
Pablo Rougerie <pablorougerie@gmail.com>
2026-04-04 16:41:04 +0200

    test: test generation spectrograms

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from params import *
+from src.params import *
 from pathlib import Path
 
 
@@ -47,30 +47,46 @@ def save_spec(spec: np.ndarray, path: Path) -> None:
     np.save(path, spec)
 
 
-def prepare_spectrograms(df: pd.DataFrame, duration: float = 600, architecture: str = "cnn") -> None:
+def prepare_spectrograms(df: pd.DataFrame, duration: float = 600, architecture: str = "cnn", force= False) -> None:
     """
     Crop, reshape, and save all spectrogram subsamples from a metadata DataFrame.
 
     Supports 'cnn', 'lstm', and 'transformer' architectures.
     Output shape: (region, freq, time) for all architectures at this stage.
     """
+
+
     if architecture not in ["cnn", "lstm", "transformer"]:
         raise ValueError(f"architecture must be 'cnn', 'lstm', or 'transformer', got '{architecture}'")
 
+    #iterates over spec
     for spec_id, group in df.groupby("spectrogram_id"):
         df_spec = load_parquet(spec_id)
 
+        #iterates over subsamples
         for _, row in group.iterrows():
+
             offset = row["spectrogram_label_offset_seconds"]
             subsample_id = row["spectrogram_sub_id"]
 
+            save_path = Path(PROCESSED_DIR / f"{spec_id}-{subsample_id}")
+
+            #avoid re-generating existing spec, unless forced
+            if save_path.exists() and force is not False:
+                print(f"spectrogram {spec_id} - subsample {subsample_id} already exists: skipped")
+                continue
+
+            #crop to right start and duration
             df_cropped = crop(df_spec, offset, duration)
+            #reshape to shape (time, region, frequencies)
             spec_array = reshape_by_region(df=df_cropped)
 
             if architecture == "cnn":
+                #reorder to (canal, frequencies, times) as needed for CNN
                 spec_out = reorder_dimensions(arr=spec_array, axes=(1, 2, 0))
             else:
                 spec_out = reorder_dimensions(arr=spec_array, axes=(1, 2, 0))
 
-            save_path = Path(PROCESSED_DIR / f"{spec_id}" / f"{subsample_id}")
+            #save subsampled spectrograms with explicit names.
+            save_path.parent.mkdir(parents=True, exist_ok=True) #create parent dir if doesnt exists
             save_spec(spec=spec_out, path=save_path)
