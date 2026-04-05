3e051e0585966a6c798cc9cabd483a661012c684
Pablo Rougerie <pablorougerie@gmail.com>
2026-04-05 12:57:39 +0200

    feat(preprocess): add tqdm reporting and fix force flag; update gitignore

--- src/params.py
+++ src/params.py
@@ -3,3 +3,4 @@ from pathlib import Path
 DATA_DIR = Path("data")
 SPEC_DIR = DATA_DIR / "train_spectrograms"
 PROCESSED_DIR = DATA_DIR / "processed"
+VOTE_COL = ["seizure_vote", "lpd_vote", "gpd_vote", "lrda_vote", "grda_vote", "other_vote"]
