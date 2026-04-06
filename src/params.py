5620528574bcbd7c31a5f15826b84e621efbe46f
Pablo Rougerie <pablorougerie@gmail.com>
2026-04-06 15:28:17 +0200

    feat: params.py comaptible kaggle

--- src/params.py
+++ src/params.py
@@ -1,8 +1,18 @@
 from pathlib import Path
 
-ROOT = Path(__file__).resolve().parent.parent  # src/ -> project root
-DATA_DIR = ROOT / "data"
-SPEC_DIR = DATA_DIR / "train_spectrograms"
-PROCESSED_DIR = DATA_DIR / "processed"
+KAGGLE = Path('/kaggle').exists()
+
+if KAGGLE:
+    ROOT = Path('/kaggle/working/brain_waves')
+    DATA_DIR = Path('/kaggle/input/hms-harmful-brain-activity-classification')
+    SPEC_DIR = DATA_DIR / 'train_spectrograms'
+    PROCESSED_DIR = Path('/kaggle/working/processed')
+    CACHE_DIR = Path('/kaggle/working/cache')
+else:
+    ROOT = Path(__file__).resolve().parent.parent
+    DATA_DIR = ROOT / 'data'
+    SPEC_DIR = DATA_DIR / 'train_spectrograms'
+    PROCESSED_DIR = DATA_DIR / 'processed'
+    CACHE_DIR = DATA_DIR / 'cache'
+
 VOTE_COL = ["seizure_vote", "lpd_vote", "gpd_vote", "lrda_vote", "grda_vote", "other_vote"]
-CACHE_DIR = DATA_DIR / "cache"
