67b1ccdb2f73f4c937eea0bf3ab457faf34ba58f
Pablo Rougerie <pablorougerie@gmail.com>
2026-04-07 10:22:23 +0200

    fix(params): update processed dir paths for local and Kaggle

--- src/params.py
+++ src/params.py
@@ -5,13 +5,13 @@ KAGGLE = Path('/kaggle').exists()
 if KAGGLE:
     DATA_DIR = Path('/kaggle/input/competitions/hms-harmful-brain-activity-classification')
     SPEC_DIR = DATA_DIR / 'train_spectrograms'
-    PROCESSED_DIR = Path('/kaggle/working/processed')
+    PROCESSED_DIR = Path('/kaggle/input/hms-processed-spectrograms')
     CACHE_DIR = Path('/kaggle/working/cache')
 else:
     ROOT = Path(__file__).resolve().parent.parent
     DATA_DIR = ROOT / 'data'
     SPEC_DIR = DATA_DIR / 'train_spectrograms'
-    PROCESSED_DIR = DATA_DIR / 'processed'
+    PROCESSED_DIR = DATA_DIR / 'processed' / 'npy_files'
     CACHE_DIR = DATA_DIR / 'cache'
 
 VOTE_COL = ["seizure_vote", "lpd_vote", "gpd_vote", "lrda_vote", "grda_vote", "other_vote"]
